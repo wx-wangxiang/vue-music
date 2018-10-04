03d6875a8f3a4c109e8ce62ea07f2ce27fb2da57
wangxiang <wangxiang@xiaomai5.com>
2018-10-04 16:13:52 +0800

    recommend list

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,24 @@
   <div id="app">
     <m-header />
     <tab />
-    <router-view></router-view>
+    <keep-alive>
+      <router-view></router-view>
+    </keep-alive>
   </div>
 </template>
 
 <script type="es6">
-  import MHeader from 'components/m-header/m-header';
-  import Tab from 'components/tab/tab';
+import MHeader from "components/m-header/m-header";
+import Tab from "components/tab/tab";
 
-  export default {
-    name: 'App',
-    components: { 
-      MHeader, 
-      Tab 
-    }
+export default {
+  name: "App",
+  components: {
+    MHeader,
+    Tab
   }
+};
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-  
 </style>
